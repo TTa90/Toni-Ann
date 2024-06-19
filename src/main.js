462d8b2da8d2aead99ecb73247180ee9e8c744ba
Toni KS <toniakileah@gmail.com>
2024-06-19 11:43:23 +0100

    $ git add --all $ git commit -a -m "$1" $ git push

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import '../styles/modern-normalise.css'
 import '../style/root.css'
 import '../style/style.css'
-import '../style/homepage.css'
+import '../style/header.css'
 import '../style/about.css'
+import '../style/project.css'
+import '../style/contact.css'
+import '../style/footer.css'
+
